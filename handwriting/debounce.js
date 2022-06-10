bcc3bb7031f812719c69c87ba84b7e425f7350b3
LuYao-pilgrim <luyao_shoottheball@163.com>
2022-06-10 20:30:37 +0800

    1

--- handwriting/debounce.js
+++ handwriting/debounce.js
@@ -1,3 +1,8 @@
+/**
+ * 防抖
+ * 在事件被触发n秒后再执行回调，如果在这n秒内又被触发，则重新计时。
+ * 个人理解 函数防抖就是法师发技能的时候要读条，技能读条没完再按技能就会重新读条。
+ */
 const debounce = (func, delay) => {
     let timer = null
 
@@ -9,3 +14,4 @@ const debounce = (func, delay) => {
         }, delay)
     }
 }
+
