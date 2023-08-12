83a7f23ea9614956705789122a472ec13594315b
LuYao-pilgrim <luyao_shoottheball@163.com>
2023-08-12 14:28:50 +0800

    2023.8.12

--- 1.js
+++ 1.js
@@ -1,57 +1,20 @@
-// 多维拍平+去除重复项
 
-
-const flat = (arr) => {
-    let res1 = []
-    arr.reduce((it, index) => {
-        res1.concat(Array.isArray(it) ? flat(it) : it)
-    },[])
-
-    return res1
-}
-
-const quchong = (arr) => {
-    let res2 = []
-    arr.map((item, index) => {
-        if(arr.indexOf(item) === index) {
-            res2.push(item)
-        }
+const promise1 = new Promise((resolve) => {
+    setTimeout(() => {
+        resolve('success')
+        console.log('timer1')
     })
+    console.log('p1内容')
+})
 
-    return res2
-}
+const promise2 = promise1.then(() => {
+    throw new Error('error!!!')
+})
 
-
-const fn = (arr) => {
-    
-    let a = flat(arr)
-    let b = quchong(a)
-
-    return b
-    
-}
-
-const array = [1,1,[2,1,[3,4,1]]]
-
-console.log(flat(array));
-
-
-
-
-
-
-
-
-
-
-
-let res = []
-
-const back = (node) => {
-    if(!node) {
-        return
-    }
-    back(node.left)
-    back(node.right)
-    res.push(node.val)
-}
+console.log('promise1', promise1)
+console.log('promise2', promise2)
+setTimeout(() => {
+    console.log('timer2')
+    console.log('promise1', promise1)
+    console.log('promise2', promise2)
+},2000)
